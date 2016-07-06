138b948c8d64d9664b2f958f29ab1dc98075e968
artificial-life <proteus.protocol@gmail.com>
2016-07-06 18:08:51 +0600

    upd

--- src/Classes/workstation-counter.js
+++ src/Classes/workstation-counter.js
@@ -3,20 +3,20 @@
 let AtomicCounter = require('./atomic-counter.js');
 
 class WorkstationCounter extends AtomicCounter {
-  static description() {
-    return {
-      "key": "cache_workstations_{department}"
-    }
-  }
-  range(query) {
-    let available_workstations = _.get(this.value, ['content', 'control-panel']);
+	static description() {
+		return {
+			"key": "cache_workstations_{department}"
+		}
+	}
+	range(query) {
+		let available_workstations = _.get(this.value, ['content', 'control-panel']);
 
-    if (query && query.state == 'active') {
-      available_workstations = _.filter(available_workstations, ['active', true]);
-    }
+		if (query && query.state == 'active') {
+			available_workstations = _.filter(available_workstations, ['state', 'active']);
+		}
 
-    return _.map(available_workstations, 'id');
-  }
+		return _.map(available_workstations, 'id');
+	}
 }
 
 module.exports = WorkstationCounter;
