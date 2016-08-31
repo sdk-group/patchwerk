a192352aa184919db810df58ddcfcf477fbe1b13
artificial-life <proteus.protocol@gmail.com>
2016-09-01 05:20:55 +0600

    attachService

--- src/classes/operator.js
+++ src/classes/operator.js
@@ -1,14 +1,22 @@
 'use strict'
 
 const BasicDocument = require('./basic.js');
+const _ = require('lodash');
 
 class Operator extends BasicDocument {
-	static description() {
-		return {
-			"key": "human-{counter}",
-			"counter": "global-membership-description"
-		};
-	}
+  static description() {
+    return {
+      "key": "human-{counter}",
+      "counter": "global-membership-description"
+    };
+  }
+  attachService(services) {
+    let provides = this.get('provides');
+
+    this.set('provides', _.concat(services, provides))
+
+    return this;
+  }
 }
 
 module.exports = Operator;
