866592064ec9f6742277f679faede651b6aab050
artificial-life <proteus.protocol@gmail.com>
2016-09-01 20:03:43 +0600

    upd

--- src/Classes/workstation-counter.js
+++ src/Classes/workstation-counter.js
@@ -12,7 +12,7 @@ class WorkstationCounter extends AtomicCounter {
 		let available_workstations = _.get(this.value, ['content', 'control-panel']);
 
 		if (query && query.state == 'active') {
-			available_workstations = _.filter(available_workstations, ['state', 'active']);
+			available_workstations = _.filter(available_workstations, ws => ws.state == 'active');
 		}
 
 		return _.map(available_workstations, 'id');
