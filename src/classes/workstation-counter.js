33a811ecc92d87dddf9e4a4f0892b66c3d0c45cd
artificial-life <proteus.protocol@gmail.com>
2016-09-16 18:29:48 +0600

    ws

--- src/classes/workstation-counter.js
+++ src/classes/workstation-counter.js
@@ -10,7 +10,19 @@ class WorkstationCounter extends AtomicCounter {
 	}
 	range() {
 		let query = this.creation_params;
-		let available_workstations = _.get(this.properties, ['content', 'control-panel']);
+		let type = this.creation_params.type || 'control-panel';
+
+		let available_workstations;
+		if (type.constructor == Array) {
+			available_workstations = _.reduce(type, (acc, single) => {
+				let items = _.get(this.properties, ['content', single]);
+				return _.concat(acc, items);
+			}, []);
+		} else {
+			available_workstations = _.get(this.properties, ['content', type]);
+		}
+
+
 
 		if (query && query.state == 'active') {
 			available_workstations = _.filter(available_workstations, ws => ws.state == 'active');
