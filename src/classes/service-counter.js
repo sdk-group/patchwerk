870c4662fd03434b33395b3375f38f79d9729741
YuuKohaku <soul.of.bytes@gmail.com>
2016-10-21 18:29:45 +0700

    upd

--- src/classes/service-counter.js
+++ src/classes/service-counter.js
@@ -2,6 +2,7 @@
 
 const _ = require('lodash');
 const AtomicCounter = require('./atomic-counter.js');
+const uuid = require("node-uuid");
 
 class ServiceCounter extends AtomicCounter {
 	static description() {
@@ -15,17 +16,17 @@ class ServiceCounter extends AtomicCounter {
 		return _.map(this.properties.content, item => item.replace('service-', ''));
 	}
 	inc(emitter) {
-		let ids = this.properties.content;
-		let last = _.last(this.properties.content);
-		let id = (parseInt(last.split('-')[1]) + 1);
+		let id = uuid.v1()
+			.replace(/-/g, "");
 
 		this.properties.content.push('service-' + id);
 
 		let data = this.getSource();
 
 		return emitter.addTask('database.upsertNodes', {
-			args: [data]
-		}).then(data => id);
+				args: [data]
+			})
+			.then(data => id);
 	}
 	add(params, patchwerk) {
 		//@NOTE: never use it without strong need
