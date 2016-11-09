fefc144e6831c0194e8667a6489c3a51c13e4bfc
YuuKohaku <soul.of.bytes@gmail.com>
2016-11-09 15:06:56 +0700

    builder

--- build.js
+++ build.js
@@ -1,3 +1,39 @@
 'use strict'
 
-require('iris-builder');
+let gulp = require("gulp");
+let babel = require("gulp-babel");
+let gutil = require("gulp-util");
+
+gulp.task("es6-js", function () {
+	let production = isProduction();
+	console.log("PROD:", production);
+	let src = ["src/**/*.js"];
+	let plg = ["transform-strict-mode"];
+	if (!production) {
+		src.push("tests/**/*.js");
+	} else {
+		// plg.push("uglify:after");
+	}
+	return gulp.src(src)
+		.pipe(babel({
+			"presets": ["es2015-node5"],
+			"babelrc": false,
+			"plugins": plg
+		}))
+		.pipe(production ? uglify() : gutil.noop())
+		.pipe(gulp.dest("build"))
+		.on('end', function () {
+			console.log('end build');
+			if (production) {
+				console.log("Deleting sources...");
+				return del(["src", "gulpfile.js", "tests"]);
+			}
+		});
+});
+
+gulp.task("json", function () {
+	return gulp.src(["src/**/*.json"])
+		.pipe(gulp.dest("build"));
+});
+
+gulp.task('default', ['es6-js', 'json']);
